f218aac8877a43fcbd5d6def95a35d0222529ccf
Alexey Panashchenko <alpa@ciklum.com>
2020-03-06 00:08:09 +0200

    Add docker-compose-db

--- project_build/initdb.sh
+++ project_build/initdb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #initdb.sh
-docker-compose -f docker-compose.yml up -d db
+docker-compose -f docker-compose-db.yml up -d db
 
-docker-compose -f docker-compose.yml run --rm dockerize -wait-retry-interval 10s -timeout 120s -wait tcp://db:5432
+docker-compose -f docker-compose-db.yml run --rm dockerize -wait-retry-interval 10s -timeout 120s -wait tcp://db:5432
 
 PGPASSWORD=proj693B119 psql -h localhost -p 5432 -U postgres -w -f initdb.sql
 #end of initdb.sh
